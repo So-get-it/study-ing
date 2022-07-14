e7b5bca462ab7c96ca0c21346645cdb6bcf6e22e
So-get-it <2472734278@qq.com>
2022-07-14 10:27:55 +0800

    have a test

---
+++ serial_port/lib/serial_port.h
@@ -0,0 +1,21 @@
+#ifndef  _SERIALPORT_H_
+#define  _SERIALPORT_H_
+
+#define SERIALNAME_LEN 128
+
+typedef struct attr_s{
+	int 		flow_ctrl;			//流控制
+	int 		baud_rate;			//波特率
+	int 		data_bits;			//数据位
+	char 		parity;				//奇偶校验位
+	int 		stop_bits;			//停止位
+}attr_t;
+
+extern int serial_open(char *fname);
+extern int serial_close(int fd, struct termios *termios_p);
+extern int serial_init(int fd, struct termios *oldtermios, attr_t attr);
+extern int serial_send(int fd, char *msg, int msg_len);
+extern int serial_recv(int fd, char *recv_msg);
+
+#endif
+
---
+++ serial_port/ttyUSBN.c
@@ -0,0 +1,175 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <termios.h>
+#include "serial_port.h"
+
+#define SERIAL_DEBUG
+
+#ifdef SERIAL_DEBUG
+#define serial_print(foriat, args...) printf(format,##args)
+#else
+#define serial_print(format, args...) do{} while(0)
+#endif
+
+void sig_stop(int signum);
+void usage();
+
+int run_stop = 0;
+
+int main()
+{
+    int 				fd, rv;
+    char 				*fname = "/dev/ttyUSB2"; // 设备节点
+    char 				buf[64] = {0};
+	char 				send_msg[128];
+	char 				recv_msg[128];
+	fd_set 				fdset;
+    struct termios 		oldtio;
+
+    
+	struct option    opts[] = {
+        {"help"    , no_argument      , NULL, 'h'},
+        {"flowctrl", required_argument, NULL, 'f'},
+        {"baudrate", required_argument, NULL, 'b'},
+        {"databits", required_argument, NULL, 'd'},
+        {"parity"  , required_argument, NULL, 'p'},
+        {"stopbits", required_argument, NULL, 's'},
+        {"name"    , required_argument, NULL, 'n'},
+        {NULL      , 0                , NULL,  0 }
+    };
+
+	while((ch = getopt_long(argc,argv,"hf:b:d:p:s:n:",opts,NULL)) != -1)
+    {
+        switch(ch)
+        {
+            case 'h':
+                usage();
+                return 0;
+
+            case 'f':
+                attr->flow_strl = atoi(optarg);
+                break;
+
+            case 'b':
+                attr->baud_rate = atoi(optarg);
+                break;
+
+            case 'd':
+                attr->data_bits = atoi(optarg);
+                break;
+
+            case 'p':
+                attr->parity = optarg;
+                break;
+
+            case 's':
+                attr->stop_bits = atoi(optarg);
+                break;
+        }
+    }
+
+    if((fd = serial_open(fname)) < 0)
+    {  
+        return -1;
+    }
+
+	if(serial_init(fd, &oldtio, attr) < 0)
+	{
+		return -2;
+	}
+
+	signal(SIGINT, sig_stop);
+	signal(SIGTERM, sig_stop);
+
+    serial_print("c_iflag: %d\n", oldtio.c_iflag);
+    serial_print("c_oflag: %d\n", oldtio.c_oflag);
+    serial_print("c_cflag: %d\n", oldtio.c_cflag);
+    serial_print("c_lflag: %d\n\n", oldtio.c_lflag);
+
+    serial_print("c_ispeed: %d\n", oldtio.c_ispeed);
+    serial_print("c_ospeed: %d\n\n", oldtio.c_ospeed);
+
+	while(!run_stop)
+    {
+        FD_ZERO(&fdset);	//清空所有文件描述符
+        FD_SET(STDIN_FILENO,&fdset);	//添加标准输入到fdset中
+        FD_SET(fd,&fdset);				//添加文件描述符fd到fdset中
+
+        /*  使用select多路复用监听标准输入和串口fd */
+        rv = select(fd + 1, &fdset, NULL, NULL, NULL);
+        if(rv < 0)
+        {
+            serial_print("Select failure......\n");
+            break;
+        }
+
+        if(rv == 0)
+        {
+            serial_print("Time Out.\n");
+            goto cleanup;
+        }
+
+		//有事件发生
+        if(FD_ISSET(STDIN_FILENO,&fdset))
+        {
+            memset(send_msg, 0, sizeof(send_msg));
+
+            /* 从标准输入读取命令 */
+            fgets(send_msg, sizeof(send_msg), stdin);
+            /* 处理要发送的数据 */
+            //adjust_buf(send_msg);
+
+			serial_print("The message will send: %s\n", send_msg);
+            if(serial_send(fd, send_msg, strlen(send_msg)) < 0)
+            {
+                serial_print("Write failed.\n");
+                goto cleanup;
+            }
+            //fflush(stdin);
+
+        }
+
+        if(FD_ISSET(fd,&fdset))
+        {
+            memset(recv_msg, 0, sizeof(recv_msg));
+
+            rv = serial_recv(fd, recv_msg);
+            if(rv <= 0)
+            {
+                serial_print("Read failed: %s\n",strerror(errno));
+                break;
+            }
+
+            serial_print("Receive data: %s",recv_msg);
+            //fflush(stdout);
+        }
+
+
+    }
+    
+cleanup:
+    close(fd);
+
+	return 0;
+}
+
+void sig_stop(int signum)
+{
+	serial_print("catch the signal: %d\n", signum);
+	run_stop = 1;
+}
+
+void usage()
+{
+    serial_print("-h(--help    ): aply the usage of this file\n");
+    serial_print("-f(--flowctrl): arguments: 0(no use) or 1(hard) or 2(soft)\n");
+    serial_print("-b(--baudrate): arguments with speed number\n");
+    serial_print("-d(--databits): arguments: 5 or 6 or 7 or 8 bits\n");
+    serial_print("-p(--parity  ): arguments: n/N(null) e/E(even) o/O(odd) s/S(space)\n");
+    serial_print("-s(--stopbits): arguments: 1 or 2 stopbits\n");
+}
